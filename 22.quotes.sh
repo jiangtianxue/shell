e22383cb62b19114d6720077cda39ab140c8c98d
LR <18297877987@163.com>
2021-11-30 04:00:01 +0800

    auto upload

---
+++ 22.quotes.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo ""
+
+current_path=/home/lr/shell
+echo "current path is ${current_path}"
+
+echo 'current path is ${current_path}'
+
+echo "today is `date +%F`"
+
+echo "today is $(date +%F)"
+
+echo "current path is $(pwd)"
+echo ""
